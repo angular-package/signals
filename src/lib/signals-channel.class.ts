eb32a5db0fb34afb64b84c605473c85d7cc9865c
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-05-17 12:48:43 +0000

    refactor: enhance documentation for SignalsChannel class and its methods

--- src/lib/signals-channel.class.ts
+++ src/lib/signals-channel.class.ts
@@ -57,13 +57,13 @@ export class SignalsChannel<
   }
 
   /**
-   * @description
+   * @description Privately holds the configuration settings for the `SignalsChannel`, including the emitter keys for events.
    * @type {SignalsSettings<EK, EV>}
    */
   #configuration: SignalsSettings<EK, EV>;
 
   /**
-   * @description
+   * @description Privately holds the subject for the `SignalsChannel`, which emits events whenever a signal is set.
    * @type {*}
    */
   #onSet  = new Subject<EmitPayload<T, EK, EV>>();
@@ -83,7 +83,7 @@ export class SignalsChannel<
   }
 
   /**
-   * @description
+   * @description Configures the `SignalsChannel` with the provided settings, allowing for customization of the emitter keys and other configuration options. This method can be used to update the configuration after the `SignalsChannel` has been instantiated.
    * @public
    * @param {SignalsSettings<EK, EV>} settings
    * @returns {this}
@@ -92,18 +92,41 @@ export class SignalsChannel<
     return (this.#configuration = settings), this;
   }
 
+  /**
+   * @description Returns an observable that emits events whenever a signal with the specified key is set. The emitted event includes the key and value of the signal, using the configured emitter keys.
+   * @public
+   * @template {keyof T} K
+   * @param {K} key
+   * @returns {Observable<{[Key in EK]: K} & {[Value in EV]: T[K]}>}
+   */
   public onSet<K extends keyof T>(key: K): Observable<{[Key in EK]: K} & {[Value in EV]: T[K]}> {
     return this.$onSet.pipe(
       filter(payload => payload[this.#configuration.emitter!.key] === key)
     ) as Observable<{[Key in EK]: K} & {[Value in EV]: T[K]}>;
   }
 
+  /**
+   * @inheritdoc
+   * @public
+   * @template {keyof T} K
+   * @param {K} key
+   * @param {(T[K] | WritableSignal<T[K]>)} value
+   * @returns {boolean}
+   */
   public override set<K extends keyof T>(key: K, value: T[K] | WritableSignal<T[K]>): boolean {
     return super.set(key, value),
       this.#emit(key, value as T[K]),
       true;
   }
 
+  /**
+   * @description Emits an event with the specified key and value using the configured emitter keys.
+   * @private
+   * @template {keyof T} S
+   * @param {S} key
+   * @param {T[S]} value
+   * @returns {this}
+   */
   #emit<S extends keyof T>(key: S, value: T[S]): this {
     return this.#onSet.next({
       [this.#configuration.emitter!.key]: key,
